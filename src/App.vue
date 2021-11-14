58c74c6eaa2f4a49a07324dfec9a684d46627c69
paulking321 <oluwadamilare321@gmail.com>
2021-11-14 23:00:02 +0100

    started

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <random-user/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import randomUser from './components/randomUser.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    randomUser
   }
 }
 </script>
---
+++ src/components/randomUser.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Random User Generator</h1>
+</template>
+
+<scipt>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
